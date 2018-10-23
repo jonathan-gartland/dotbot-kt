870085d3fd947f036aa23f4e205d9c4cd11479da
Steven Knight <steven.knight@cashstar.com>
2018-10-23 10:24:47 -0400

    Add building of fat-jar file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+
 plugins {
     kotlin("jvm") version "1.2.71"
     id("org.jmailen.kotlinter") version "1.20.1"
@@ -15,14 +16,26 @@ repositories {
     mavenCentral()
 }
 
-
 dependencies {
     compile(kotlin("stdlib-jdk8"))
     compile(group = "com.github.ajalt", name = "clikt", version = "1.5.0")
 }
 
-
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+val fatJar = task("fatJar", type = Jar::class) {
+    baseName = "${project.name}-fat"
+    manifest {
+        attributes["Main-Class"] = "dotbotKt.MainKt"
+    }
+    from(configurations.runtime.map({ if (it.isDirectory) it else zipTree(it) }))
+    with(tasks["jar"] as CopySpec)
+}
+
+tasks {
+    "build" {
+        dependsOn(fatJar)
+    }
+}
