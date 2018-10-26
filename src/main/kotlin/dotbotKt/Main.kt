6470f84b92cc10980add0656f37ce265eaf5ed80
Steven Knight <steven.knight@cashstar.com>
2018-10-26 19:59:38 -0400

    Add remaining options from dotbot.

--- src/main/kotlin/dotbotKt/Main.kt
+++ src/main/kotlin/dotbotKt/Main.kt
@@ -5,29 +5,61 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.*
 
 data class DotBotKt(val baseDir: String?, val configFile: String?, val superQuiet: Boolean=false,
-                    val quiet: Boolean=false, val verbose: Boolean=false)
+                    val quiet: Boolean=false, val verbose: Boolean=false,
+                    val plugin: String?,
+                    val pluginDir: String?,
+                    val disableBuiltInPlugins: Boolean=false,
+                    val noColor: Boolean=false)
 
 class Cli : CliktCommand(
         help = """
-            test.
+            Dotbot is a tool that bootstraps your dotfiles (it's a [Dot]files [bo]o[t]strapper, get it?). It does
+            less than you think, because version control systems do more than you think.
+
+            Dotbot is designed to be lightweight and self-contained, with no external dependencies and no installation
+            required. Dotbot can also be a drop-in replacement for any other tool you were using to manage your dotfiles,
+            and Dotbot is VCS-agnostic -- it doesn't make any attempt to manage your dotfiles.
         """.trimIndent()) {
     init {
-        versionOption("0.0")
+        versionOption("0.0.1")
     }
 
-    val baseDir: String by option("--base-directory", "-b",
-            help = "execute commands from within").default("BASEDIR")
-    val configFile: String? by option("--config-file", "-c",
-            help = "run commands given in CONFIGFILE")
-    val superQuiet: Boolean by option("--super-quiet", "-Q",
+    private val _basedir  = "BASEDIR";
+    private val _plugin = "PLUGIN";
+    private val _pluginDir = "PLUGIN_DIR";
+
+    private val baseDir: String by option("--base-directory", "-b",
+            help = "execute commands from within").default(this._basedir)
+
+    private val superQuiet: Boolean by option("--super-quiet", "-Q",
             help ="suppress almost all output").flag()
-    val quiet: Boolean by option("--quiet", "-q",
+
+    private val quiet: Boolean by option("--quiet", "-q",
             help = "suppress most output").flag()
-    val verbose: Boolean by option("--verbose", "-v",
+
+    private val verbose: Boolean by option("--verbose", "-v",
             help = "enable verbose output").flag()
 
+    private val noColor: Boolean by option("--no-color",
+            help = "disable color output").flag()
+
+    private val configFile: String? by option("--config-file", "-c",
+            help = "run commands given in CONFIGFILE")
+
+    private val plugin: String by option("--plugins", "-p",
+            help = "load PLUGIN as a plugin").default(this._plugin);
+
+    private val pluginDir: String by option("--plugin-dir",
+            help = "load all plugins in PLUGIN_DIR").default(this._pluginDir);
+
+    private val disableBuiltInPlugins: Boolean by option("--disable-built-in-plugins",
+            help = "disable built-in plugins").flag()
+
     override fun run() {
-        val dotBotKt = DotBotKt(baseDir, configFile, superQuiet, quiet, verbose)
+        val dotBotKt = DotBotKt(baseDir, configFile,
+                superQuiet, quiet, verbose,
+                plugin, pluginDir, disableBuiltInPlugins,
+                noColor)
         context.obj = dotBotKt
     }
 }
